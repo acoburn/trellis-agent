33c42dcc0102aa809899f38c7354b0337a950c6c
Aaron Coburn <acoburn@amherst.edu>
2017-01-16 14:16:37 -0500

    update build script

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject trellis-service-agent-json "0.1.0-SNAPSHOT"
-  :description "And agent service for a trellis repository"
-  :url "http://example.com/FIXME"
+(defproject trellis-agent "0.1.0-SNAPSHOT"
+  :description "An agent service for a trellis repository"
+  :url "https://github.com/acoburn/trellis"
   :license {:name "Apache 2"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
                  [org.clojure/clojure "1.8.0"]
                  [org.apache.commons/commons-rdf-api "0.3.0-incubating"]
